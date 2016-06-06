277639adb06dedb8a4a007dc7804b083b548af56
jonathangunnells <jonathan.gunnells@yahoo.com>
2016-06-06 12:04:49 -0400

    Display user history

--- src/com/theironyard/Main.java
+++ src/com/theironyard/Main.java
@@ -4,6 +4,8 @@ import spark.ModelAndView;
 import spark.Spark;
 import spark.template.mustache.MustacheTemplateEngine;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.HashMap;
 
 //route: particular end point
@@ -13,6 +15,7 @@ import java.util.HashMap;
 public class Main {
 
     static User user;
+    static ArrayList<User> userList = new ArrayList<>();
 
     public static void main(String[] args) {
         Spark.init();
@@ -26,6 +29,7 @@ public class Main {
                     }
                     else {
                         m.put("name", user.name);
+                        m.put("users", userList);
                         return new ModelAndView(m, "home.html");
                     }
                 },
@@ -37,10 +41,19 @@ public class Main {
                 (request, response) -> {
                     String username = request.queryParams("username");
                     user = new User(username);
+                    userList.add(user);
                     response.redirect("/");
                     return "";
 
                 }
+        );
+        Spark.post(
+                "/logout",
+                        (request, response) -> {
+                            user = null;
+                            response.redirect("/");
+                            return "";
+                             }
         );
     }
 
