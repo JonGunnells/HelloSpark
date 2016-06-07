49a45b692a29cf5d4fba9e1be519640aca26ede1
jonathangunnells <jonathan.gunnells@yahoo.com>
2016-06-07 10:30:55 -0400

    Added sessions

--- src/com/theironyard/Main.java
+++ src/com/theironyard/Main.java
@@ -1,6 +1,7 @@
 package com.theironyard;
 
 import spark.ModelAndView;
+import spark.Session;
 import spark.Spark;
 import spark.template.mustache.MustacheTemplateEngine;
 
@@ -14,21 +15,24 @@ import java.util.HashMap;
 
 public class Main {
 
-    static User user;
+    static HashMap<String, User> users = new HashMap<>();
     static ArrayList<User> userList = new ArrayList<>();
 
     public static void main(String[] args) {
         Spark.init();
         Spark.get(
                 "/", //slash route, 1st argument
-                (request, response) -> {    //2nd argument
+                (request, response) -> {
+                    Session session = request.session();
+                    String username = session.attribute("username");
+
                     HashMap m = new HashMap();
-                    if (user == null) {
+                    if (username == null) {
                         return new ModelAndView(m, "login.html");
 
                     }
                     else {
-                        m.put("name", user.name);
+                        m.put("name", username);
                         m.put("users", userList);
                         return new ModelAndView(m, "home.html");
                     }
@@ -40,8 +44,16 @@ public class Main {
                 "/login",
                 (request, response) -> {
                     String username = request.queryParams("username");
-                    user = new User(username);
-                    userList.add(user);
+                    User user = users.get(username);
+                    if (user == null) {
+                        user = new User(username);
+                        users.put(username, user);
+                        userList.add(user);
+                    }
+
+                    Session session = request.session();
+                    session.attribute("username", username);
+
                     response.redirect("/");
                     return "";
 
@@ -50,7 +62,8 @@ public class Main {
         Spark.post(
                 "/logout",
                         (request, response) -> {
-                            user = null;
+                            Session session = request.session();
+                            session.invalidate();
                             response.redirect("/");
                             return "";
                              }
